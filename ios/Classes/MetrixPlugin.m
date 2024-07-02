8ae1a4878d951d3ba7232dad5b36e66ef69b09c1
Salman Qadami <salmanqadami@gmail.com>
2024-07-02 13:37:40 +0330

    change metrix_plugin to metrix_analytics

--- ios/Classes/MetrixPlugin.m
+++ ios/Classes/MetrixPlugin.m
@@ -1,11 +1,11 @@
 #import "MetrixPlugin.h"
-#if __has_include(<metrix_plugin/metrix_plugin-Swift.h>)
-#import <metrix_plugin/metrix_plugin-Swift.h>
+#if __has_include(<metrix_analytics/metrix_analytics-Swift.h>)
+#import <metrix_analytics/metrix_analytics-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "metrix_plugin-Swift.h"
+#import "metrix_analytics-Swift.h"
 #endif
 
 @implementation MetrixPlugin
--- ios/metrix_analytics.podspec
+++ ios/metrix_analytics.podspec
@@ -1,9 +1,9 @@
 #
 # To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html.
-# Run `pod lib lint metrix_plugin.podspec` to validate before publishing.
+# Run `pod lib lint metrix_analytics.podspec` to validate before publishing.
 #
 Pod::Spec.new do |s|
-  s.name             = 'metrix_plugin'
+  s.name             = 'metrix_analytics'
   s.version          = '1.4.0'
   s.summary          = 'Metrix Flutter plugin.'
   s.description      = <<-DESC
